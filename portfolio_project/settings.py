3dbdcd715e5e71710e2d1bd421934082f635dc78
nupurdharpure <153537042+nupurdharpure@users.noreply.github.com>
2025-07-29 21:40:03 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',
+    'portfolio.onrender.com'
 ]
 
 # Add CSRF trusted origins for Render domains
